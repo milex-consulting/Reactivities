b67bbfca94f36cd48f96b491ed35bdcc32cd5015
Lex <dev-ops@milexconsulting.uk>
2024-01-17 16:09:18 +0000

    Adding semantic UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0) 
+  const[activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+    .then (response => {
+      setActivities(response.data)
+    })
+  }, [])
 
   return (
+    <div>
     <h1>Reactivities</h1>
+    <ul>
+      {activities.map((activity: any) => (
+        <li key = {activity.id}>
+          {activity.title}
+        </li>
+      ))}
+
+    </ul>
+    </div>
   )
 }
 export default App

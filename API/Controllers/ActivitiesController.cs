5a9a82fc294aeba7ccc6c6f85080bcf2f00c0e0c
Lex <dev-ops@milexconsulting.uk>
2024-01-20 12:08:26 +0000

    Mediatr installation and testing

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,31 +1,39 @@
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
        
-        private readonly DataContext _context;
+        // private readonly DataContext _context; after adding MediatR
+        private readonly IMediator _mediator;
         
-        public ActivitiesController(DataContext context)
+        public ActivitiesController(IMediator mediator)
         {            
-            _context = context;
+            _mediator = mediator;            
         }   
 
         [HttpGet] //api/activities 
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
         
         [HttpGet("{id}")] //api/activities/guid
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return Ok();
         }
+
+        // [HttpPost("{id}")] //api/activities/Edit/{id}
+        // public async Task<ActionResult<Activity>> EditActivity(Guid id)
+        // {
+        //     return await _context.Activities.FindAsync(id);
+        // }
 
     }
 }
